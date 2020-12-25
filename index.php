2dfa466d255ef43eba7618404dd9f110f0693451
angel <andresbjar97@gmail.com>
2020-12-25 13:14:15 -0500

    test comandos

---
+++ index.php
@@ -0,0 +1,25 @@
+
+	
+<html>
+	<head>
+		<title>Administrar Libro</title>
+	</head>	
+	<body>
+		<header>
+			Bienvenido Administrar Libro
+		</header>
+		
+		<table border=1>			
+			<tr>
+				<td><a href="ingresar.php">Ingresar</a></td>
+			</tr>
+			<tr>
+				<td><a href="mostrar.php">Ver</a></td>
+			</tr>
+		</table>
+		
+		<footer>
+			Administrar Libro 2017
+		</footer>
+	</body>
+</html>
